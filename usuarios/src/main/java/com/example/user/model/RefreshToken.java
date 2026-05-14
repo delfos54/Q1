14254b8cbe28e835122662efd0a30908de293246
delfos54 <humancaileo@gmail.com>
2026-05-14 18:36:04 -0400

    Estructura Monorepo: integración de microservicios de producto y usuarios

--- usuarios/src/main/java/com/example/user/model/RefreshToken.java
+++ usuarios/src/main/java/com/example/user/model/RefreshToken.java
@@ -1,5 +1,23 @@
 package com.example.user.model;
 
-public class RefreshToken {
+import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class RefreshToken {
+ @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    private String username;
+
+    private Date expiryDate;
 }
--- usuarios/src/main/java/com/example/user/model/user.java
+++ usuarios/src/main/java/com/example/user/model/user.java
@@ -1,5 +1,23 @@
 package com.example.user.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class user {
+    @Id 
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long id;
+    @Column( unique = true)
+    private String username;
+    private String password;
+    private String role;
+
 
 }
