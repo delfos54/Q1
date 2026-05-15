ee2ccc0373d13b9df61323cf7f9fcef70b1678b8
delfos54 <humancaileo@gmail.com>
2026-05-15 04:24:53 -0400

    actualizacion2

--- pago/src/main/java/com/example/pago/model/pago.java
+++ pago/src/main/java/com/example/pago/model/pago.java
@@ -7,18 +7,29 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-
+@Table(name = "pagos")
 public class pago {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;    
+
+    @Column(name = "usuario_id", nullable = false)
     private String usuarioId;
+    @Column(name = "producto_id")
     private long productoId;
+
     private Integer cantidad;
+
+    @Column(name = "monto_total")
     private BigDecimal montoTotal;
+
+    @Column(name = "metodo_pago")
     private String metodoPago;
+
+
     private String Estado;
+ @Column(name = "fecha_pago") 
     private LocalDateTime fechaPago;
     @PrePersist
     protected void onCreate(){
--- user/src/main/java/com/example/user/security/jwtUtil.java
+++ user/src/main/java/com/example/user/security/jwtUtil.java
@@ -4,11 +4,11 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
-
+@Component  
 public class jwtUtil {
  private final Key key;
 
